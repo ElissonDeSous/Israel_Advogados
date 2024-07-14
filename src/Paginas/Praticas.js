975177ea3724fb6e158ba4599741ec178a9a5503
unknown <elissondsousa@gmail.com>
2024-07-13 22:56:59 -0300

    ajustando responsividade

--- src/Paginas/Praticas.js
+++ src/Paginas/Praticas.js
@@ -4,15 +4,15 @@ function Praticas (){
     return(
         <div>
    
-           <div className="flex flex-col justify-center items-center lg:h-[1300px] m-[20px]: h-[1800px]">
+           <div className="flex flex-col justify-center items-center lg:h-[1300px] m-[20px]: h-[2400px]">
                <h2 className="lg:text-4xl">Áreas de atuação</h2>
                <p>Serviços oferecidos</p>
 
-               <div className="mt-[50px] lg:flex justify-around lg:flex-wrap lg:w-[850px]">
+               <div className="mt-[50px] lg:flex justify-around lg:flex-wrap lg:w-[1100px]">
 
                 <div>
-                <ol className=" flex flex-col items-center ">
-                        <h2 className=" text-2xl font-bold flex justify-center lg:m-2">Direito civil</h2>
+                <ol className=" flex flex-col items-center mb-7">
+                        <h2 className=" text-2xl font-bold flex justify-center  ">Direito civel</h2>
                              <li className="">Direito das Obrigações</li>
                              <li>Direito Contratual</li>
                              <li>Direito de Familia</li>
@@ -20,20 +20,20 @@ function Praticas (){
                              <li>Direito das Coisas</li>
                              <li>Direito Imobiliario</li>
                     </ol>
-                  <ol className="flex flex-col items-center lg:m-2 ">
+                  <ol className="flex flex-col items-center lg:m-2 mb-7 ">
                         <h2 className=" text-2xl font-bold flex justify-center ">Direito Penal</h2>
                              <li className="">Direito Penal Substantivo</li>
                              <li>Direito Penal Processual</li>  
                     </ol>
 
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center ">Direito Trabalhalista</h2>
                              <li>Direito Individual do trabalho</li>
                              <li>Direito Coletivo do Trabalho</li>
                              <li>Direito Processual do Trabalho</li>
                             
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center ">Direito Administrativo</h2>
                     
                           <li>Licitações e Contratos Administrativos</li>
@@ -41,7 +41,7 @@ function Praticas (){
                              <li>Direito Urbanístico</li>
                            
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center ">Direito Constitucional</h2>
                     
                           <li>Controle de Constitucionalidade</li>
@@ -49,7 +49,7 @@ function Praticas (){
                              <li>Organização do Estado</li>
                            
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-1">
+                    <ol className="flex flex-col items-center lg:m-1 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Empresarial</h2>
                     
                           <li>Direito Societário</li>
@@ -58,76 +58,81 @@ function Praticas (){
                           <li>Direito Concorrencia</li>
                            
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Tributário</h2>
                           <li>Tributos Federais</li>
                           <li>Tributos Estaduais</li>
                           <li>Tributos Municipais</li>
                            
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                  
+                    
+                </div>
+               
+                    <div>
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Internacional</h2>
                           <li>Direito Internacional Publico</li>
                           <li>Direito Internacional Privado </li>
                           <li>Direito Humanos Internacionais</li>
                            
                     </ol>
-                </div>
-                 
-                    <div>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Ambiental</h2>
                           <li>Legislação Ambiental</li>
                           <li>Direito Minerario</li>
                           <li>Direito dos cursos Hidricos</li>
                            
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Consumidor</h2>
                           <li>Proteção de Defesa do Consumidor</li>
                           <li>Relações de Consumo</li>
                            
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Previdenciário</h2>
                           <li>Seguridade Social</li>
                           <li>Benefícios Previdenciarios</li>
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Militar</h2>
                           <li>Direito Penal Militar</li>
                           <li>Direito Administrativo Militar</li>
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Digital</h2>
                           <li>Direito da Internet</li>
                           <li>Proteção de Dados Pessoais</li>
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Financeiro Econômico</h2>
                           <li>Mercado de Capitais</li>
                           <li>Direito Bancario</li>
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-2">
+                    <ol className="flex flex-col items-center lg:m-2 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito de Energia</h2>
                           <li>Regulação de Energia Elétrica</li>
                           <li>Direito do Petroleo e Gás</li>
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-6">
+                    
+                    
+                    </div>
+                   
+                    <div>
+                   
+                    <ol className="flex flex-col items-center lg:m-6 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Marítimo e Aeronáutico
                         </h2>
                           <li>Regulação Maritima</li>
                           <li>Direito Aeronáutico</li>
                     </ol>
-                    <ol className="flex flex-col items-center lg:m-6">
+                    <ol className="flex flex-col items-center lg:m-6 mb-7">
                         <h2 className=" text-2xl font-bold flex justify-center">Direito Agrário</h2>
                           <li>Reforma Agrária</li>
                           <li>Direito Fundiario</li>
                     </ol>
-                    
-                    </div>
-                   
-
+                      </div>
                </div>
            </div>
 
--- src/componentes/Topo.js
+++ src/componentes/Topo.js
@@ -9,13 +9,13 @@ function Topo(){
     }
     return(
         <div>
-            <header id="topo" className=" w-[100%] h-[100px] bg-cyan-950 md: fle-col flex  justify-between text-white ">
+            <header id="topo" className=" w-[100%] md:[100%] h-[100px] bg-cyan-950 md: fle-col flex  justify-between text-white ">
             <div className="h-[100px] text-2xl    flex justify-center w-[300px] items-center">
                 <h1 className="md:ext-3xl ">Israel Advogados</h1>
               </div>
            <nav className="h-600px bg-cyan-950  ">
             <div className="bg-cyan-950  w-full  ">
-             <ul className=" md:flex justify-around  items-center h-[100px] w-[700px] hidden ">
+             <ul className=" md:flex justify-around  items-center h-[100px] w-[700px] hidden md:hidden ">
              <li>
                     <Link to = "/" >Página Inicial</Link>
                 </li>
@@ -36,7 +36,7 @@ function Topo(){
                 </li>
                 </ul>
           
-           <div className=" md:hidden mr-2  w-[100%] flex ">
+           <div className=" lg:hidden mr-2  w-[100%] flex ">
             <button onClick={Menu} id="menuBotao" type="button" className="inline-flex items-center" >
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 128 128">
                     <path fill="#fff" d="M64 14A50 50 0 1 0 64 114A50 50 0 1 0 64 14Z"></path><path fill="#444b54" d="M64,117c-29.2,0-53-23.8-53-53s23.8-53,53-53s53,23.8,53,53S93.2,117,64,117z M64,17c-25.9,0-47,21.1-47,47s21.1,47,47,47s47-21.1,47-47S89.9,17,64,17z"></path><path fill="#444b54" d="M86.5 52h-45c-1.7 0-3-1.3-3-3s1.3-3 3-3h45c1.7 0 3 1.3 3 3S88.2 52 86.5 52zM86.5 67h-45c-1.7 0-3-1.3-3-3s1.3-3 3-3h45c1.7 0 3 1.3 3 3S88.2 67 86.5 67z"></path><g><path fill="#444b54" d="M86.5,82h-45c-1.7,0-3-1.3-3-3s1.3-3,3-3h45c1.7,0,3,1.3,3,3S88.2,82,86.5,82z"></path></g>
@@ -44,7 +44,7 @@ function Topo(){
             </button>
          
            </div>
-           <div id="menu" className=" border-solid border-2 border-black  md:hidden hidden relative: right-[0px] h-[400px] absolute  bg-cyan-950 flex flex-col  justify-around items-center w-[100%] ">
+           <div id="menu" className=" border-solid border-2 border-black  lg:hidden hidden relative: right-[0px] h-[400px] absolute  bg-cyan-950 flex flex-col  justify-around items-center w-[100%] ">
                <div className=" px-2 pt-2 pd-3 sm:px-3 flex justify-center items-center ">
                 <ul className="   flex items-center flex-col h-[400px] w-[100%]   text-white justify-around ">
                 <li>
