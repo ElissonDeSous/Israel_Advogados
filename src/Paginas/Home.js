381cbb62bd9f8988b37fedaac4a7c62f5388b955
unknown <elissondsousa@gmail.com>
2024-07-16 18:54:35 -0300

    alterações na home

--- src/Paginas/Home.js
+++ src/Paginas/Home.js
@@ -1,4 +1,5 @@
 import banner from "./img/banner.jpg"
+import imgDireito1 from "./img/imagemDireitocivel.jpg"
 
 function Home (){
     return(
@@ -11,7 +12,7 @@ function Home (){
         <div className="h-[300px] w-full  flex flex-col  justify-center items-center">
           <h2 id="h2" className="md:text-7xl text-3xl font-serif ">Israel Advogados</h2>
          <p className="m-[20px] md:text-2xl text-1xl">OAB/SP - 464.844</p>
-                <p className=" m-[20px] md:text-2xl text-1xl">O direito é a busca pela verdade!</p>
+                <p className=" m-[20px] md:text-2xl ">O Direito não só é uma busca pela Justiça, mas <span className="flex justify-center">  também uma busca pela verdade!</span></p>
         </div>
 
         <div className="bg-cyan-950 h-[300px] lg:flex lg:flex-col flex flex-col justify-center items-center text-white ">
@@ -22,10 +23,10 @@ function Home (){
         </div>
         <div className="lg:flex justify-center ">
             <div className="flex justify-center">
-                <img src="https://direito.usp.br/pca/arquivos/e83584365b58_justica.jpg" className=" w-[100%]  lg:w-[490px] lg:h-[326px] md:100%"  alt=""/>
+                <img src= {imgDireito1} className=" w-[100%]  lg:w-[490px] lg:h-[326px] md:100%"  alt=""/>
             </div>
             <div className="   flex flex-col justify-center items-center w-[350px] md:w-[100%] m-[20px] lg:w-[400px] lg:h-[327px">
-               <h2 className="text-2xl">Direito Cível
+               <h2 className="text-2xl font-bold" >Direito Cível
                </h2>
                <p className="w-[300px] m-[20px] text-justify ">
                Com vasta experiência na área do Direito Cível, nosso escritório tem um histórico comprovado de sucesso em diversos casos. Atendemos clientes em São Paulo e em todo o Brasil, com a máxima eficiência e celeridade, sempre focados em alcançar os melhores resultados legais para nossos clientes.
@@ -39,19 +40,19 @@ function Home (){
         </div>
         <div className="  lg:flex  lg:justify-center  justify-center">
             <div className="  flex m-[20px] flex-col sm:justify-normal justify-center items-center  w-[350px] md:w-[100%] lg:w-[400px] lg:[327]">
-                <h2 className="text-2xl m-[20px flex justify-center lg:justify-center w-[300px] lg:w-[350px] ">Direito Trabalhista
+                <h2 className=" font-bold text-2xl m-[20px flex justify-center lg:justify-center w-[300px] lg:w-[350px] ">Direito Trabalhista
                 </h2>
                 <p className="w-[300px] m-[20px] text-justify " >
                 Especializados em Direito Trabalhista, acumulamos anos de prática bem-sucedida nesta área. Atuamos em São Paulo e em todo o território nacional, prestando serviços com agilidade e eficácia. Nossos clientes confiam em nossa expertise para conduzir processos trabalhistas, visando sempre a proteção de seus direitos e interesses de maneira exemplar
                 </p>
                 
              </div>
-             <div className="flex w-full lg:w-[500px] ">
-                <img src="https://wp.ebradi.com.br/wp-content/uploads/2020/07/ebradi_ebradi_image_467-1080x675.jpeg" className=" w-[100%] md:w-[100%] lg:w-[460px] lg:h-[327]  "  alt=""/>
+             <div className="flex w-full lg:w-[420px] relative right-[20px]">
+                <img src="https://wp.ebradi.com.br/wp-content/uploads/2020/07/ebradi_ebradi_image_467-1080x675.jpeg" className="  w-[100%] md:w-[100%] lg:w-[450px] lg:h-[327]  "  alt=""/>
              </div>
-             <div className="  flex flex-col justify-center items-center  w-[350px] md:w-[100%] lg:w-[400px] ">
-                <h2 className="  text-2xl flex justify-center w-[350px] lg:justify-center ">Direito de danos pessoais</h2>
-                <p className="  relative: left-[200px] w-[350px] m-[20px] flex justify-center text-justify">
+             <div className="  flex flex-col items-center  w-[350px] md:w-[100%] lg:w-[400px] ">
+                <h2 className=" font-bold text-2xl m-[20px] flex justify-center lg:justify-center w-[300px] lg:w-[350px] mt-[20px] ">Direito de Danos Pessoais</h2>
+                <p className="   w-[300px]  flex justify-center text-justify">
               
                 O Israel Advogados tem uma equipe de especialistas prontos para assessorar os clientes na área de São Paulo em questões relacionadas a Direito de danos pessoais. Estendemos nossas experiências jurídicas para ajudar a atender clientes de todos os tipos e tamanhos. Ligue para nós agora mesmo.
 
@@ -61,7 +62,7 @@ function Home (){
 
              
         </div>
-        <div className="flex justify-center h-[100px] items-center text-3xl">
+        <div className="flex justify-center h-[100px] items-center font-bold lg:text-3xl">
                 <h2> Atuação em todas as áreas do Direito</h2>
 
              </div>
