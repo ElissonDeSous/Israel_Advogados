f5350a0073e81a75edce01aa583161382230adb7
unknown <elissondsousa@gmail.com>
2024-07-19 03:15:51 -0300

    alterando contato

--- src/Paginas/Contato.js
+++ src/Paginas/Contato.js
@@ -1,8 +1,10 @@
+import Banner from ".././assets/imagemcontato.png"
+
 function Contato(){
   return(
     <div>
-     <section className=" mt-[20px] flex justify-center">
-           <div className="flex  flex-col lg:items-center  border-solid border-2 border-black   lg:w-[400px]   ">
+     <section className=" mt-[20px] flex flex-col items-center justify-center">
+           <div className="flex  flex-col lg:items-center     lg:w-[400px]   ">
             <h2 className=" text-4xl flex justify-center m-[20px]">Entre em contato</h2>
             <div className="h-[200px] flex flex-col  w-[400px]">
             <div className="flex m-2   items-center">
@@ -22,13 +24,15 @@ function Contato(){
                 <img className="w-[50px]" src="https://icones.pro/wp-content/uploads/2021/03/icone-linkedin-ronde-originale.png"/>
                 <p className="m-2">Israel Oliveira Pereira</p>
               </a>
+            
                 
              </div>
-             <div className="flex m-2 items-center">
-                <a href="https://israeladvogados.com/agendamento" target="_blank"><img className="w-[50px]" src="https://static-00.iconduck.com/assets.00/outlook-icon-1024x1024-5jgkdjd0.png" alt=""/></a>
-                <p className="m-2">israel.advocacia@outlook.com
-                </p>
-             </div>
+             <a className="flex relative left-2 "   href="https://israeladvogados.com/agendamento">
+                <img className="w-[50px]" src="https://static-00.iconduck.com/assets.00/outlook-icon-1024x1024-5jgkdjd0.png"/>
+                <p className="m-2">Israel Oliveira Pereira</p>
+              </a>
+            
+           
              
            </div>
           
@@ -36,6 +40,9 @@ function Contato(){
          
                  <a href="https://wa.me/12997917063?text=Olá, Dr. Israel! Gostaria de marcar uma consulta." target="_blank"><img className="w-[50px]" src= "https://imagepng.org/wp-content/uploads/2017/08/whatsapp-icone-1.png" alt=""/></a>
           </div>
+            </div>
+            <div>
+             <img width="900px" src={Banner} />
             </div>
                     
         </section>
